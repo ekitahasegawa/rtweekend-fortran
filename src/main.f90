574a62892b8240d2e6a2156a747b138d820252bb
EkitaHasegawa <hasegawaekita@gmail.com>
2023-07-17 10:41:24 -0500

    Experimental implementation of binary PPM format

--- src/main.f90
+++ src/main.f90
@@ -1,32 +1,43 @@
 program main
-   use rtweekend, only : rk
+   use rtweekend, only : rk, convert_to_unsigned, newln
+   use iso_fortran_env, only : int8,int64
    implicit none
 
-   integer, parameter :: image_width = 256, image_height=256
+   integer, parameter :: image_width = 256, image_height=256, max_pixel_val = 255
    character(len=*), parameter :: filename="image.ppm"
    integer :: lun, ii, jj
+   integer(int8), dimension(:,:,:), allocatable :: pixel_field
 
    integer :: ir,ig,ib
-   real :: r,g,b
-   
-   open(newunit=lun,file=filename,status="replace",action="write")
-   write(lun,"(A)") "P3"
-   write(lun,"(I0,X,I0)") image_width, image_height
-   write(lun,"(A)") "255"
+   real(rk) :: r,g,b
+
+   allocate(pixel_field(3,image_width,image_height))
 
    do jj=1,image_height
       do ii=1,image_width
-         r = real(ii,kind=kind(r))/(image_width-1)
-         g = real(jj,kind=kind(g))/(image_height-1)
-         b = 0.25_rk
+         r = real((ii-1),kind=rk)/(image_width)
+         g = real((jj-1),kind=rk)/(image_height)
+         b = 0.5_rk
 
          ir = int(256.0_rk * r)
          ig = int(256.0_rk * g)
          ib = int(256.0_rk * b)
 
-         write(lun,"(I0,X,I0,X,I0)") ir, ig, ib
+         pixel_field(:,ii,jj) = convert_to_unsigned([ir,ig,ib])
       end do
    end do
 
+   open(newunit=lun,file=filename,status="replace",action="write",&
+   form="formatted")
+
+   write(lun,"(A)") "P6"
+   write(lun,"(I0,X,I0)") image_width, image_height
+   write(lun,"(A)") "255"
+   close(lun)
+
+   open(newunit=lun,file=filename,status="old",action="write",&
+   form="unformatted",position="append")
+
+   write(lun) pixel_field
    close(lun)
 end program main
--- src/rtweekend.f90
+++ src/rtweekend.f90
@@ -1,4 +1,35 @@
 module rtweekend
+   use iso_fortran_env, only : int8,int32,int64
    implicit none
    integer, parameter :: rk = selected_real_kind(15)
+   character, parameter :: newln = new_line("A")
+
+   contains
+
+   elemental function convert_to_unsigned(n) result(n_u)
+      integer, value :: n
+      integer(int8) :: n_u
+      integer :: n_set_upper_bits
+
+      integer :: ii,bit_mask
+      character(len=32) :: err
+
+      bit_mask = ibits(n,bit_size(n_u),bit_size(n)-bit_size(n_u))
+
+      n_set_upper_bits = popcnt(bit_mask)
+
+      if(n_set_upper_bits.gt.0) then
+         write(err,*) n
+         error stop "ERROR: BITWISE VALUE "//trim(adjustl(err))//" CANNOT FIT IN 8 BIT UNSIGNED INT"
+      end if
+
+      n_u = 0
+
+      do ii = bit_size(n_u)-1,0,-1
+         if(n.ge.(2**ii)) then
+            n_u = ibset(n_u,ii)
+            n = n - 2**ii
+         end if
+      end do
+   end function convert_to_unsigned
 end module rtweekend
