8be7c32f10eb807ff50b7d5f66f196a02970b2f4
EkitaHasegawa <hasegawaekita@gmail.com>
2023-07-17 11:32:09 -0500

    Implemented binary PPM output. TODO: Binary PPM includes automatic gamma correction. Need to compensate.

--- src/main.f90
+++ src/main.f90
@@ -1,12 +1,12 @@
 program main
    use rtweekend, only : rk, convert_to_unsigned, newln
-   use iso_fortran_env, only : int8,int64
+   use iso_fortran_env, only : int8,int32,int64,real32,real64
    implicit none
 
    integer, parameter :: image_width = 256, image_height=256, max_pixel_val = 255
    character(len=*), parameter :: filename="image.ppm"
-   integer :: lun, ii, jj
-   integer(int8), dimension(:,:,:), allocatable :: pixel_field
+   integer :: ii, jj
+   integer(int32), dimension(:,:,:), allocatable :: pixel_field
 
    integer :: ir,ig,ib
    real(rk) :: r,g,b
@@ -17,27 +17,75 @@ program main
       do ii=1,image_width
          r = real((ii-1),kind=rk)/(image_width)
          g = real((jj-1),kind=rk)/(image_height)
-         b = 0.5_rk
+         b = 0.25_rk
 
          ir = int(256.0_rk * r)
          ig = int(256.0_rk * g)
          ib = int(256.0_rk * b)
 
-         pixel_field(:,ii,jj) = convert_to_unsigned([ir,ig,ib])
+         pixel_field(:,ii,jj) = [ir,ig,ib]
       end do
    end do
 
-   open(newunit=lun,file=filename,status="replace",action="write",&
-   form="formatted")
+   call write_ppm_ascii("ascii_image.ppm",pixel_field)
+   call write_ppm_binary("binary_image.ppm",pixel_field)
 
-   write(lun,"(A)") "P6"
-   write(lun,"(I0,X,I0)") image_width, image_height
-   write(lun,"(A)") "255"
-   close(lun)
+   contains
 
-   open(newunit=lun,file=filename,status="old",action="write",&
-   form="unformatted",position="append")
+   subroutine write_ppm_ascii(filename,data,pixel_max)
+      character(len=*), intent(IN) :: filename
+      integer, dimension(:,:,:), intent(IN) :: data
+      integer, intent(IN), optional :: pixel_max
 
-   write(lun) pixel_field
-   close(lun)
+      integer :: lun, image_height, image_width, pmax, ii, jj
+
+      pmax = 255
+      if(present(pixel_max)) pmax = pixel_max
+
+      open(newunit=lun,file=filename,status="replace",action="write")
+
+      image_width = size(data,2)
+      image_height = size(data,3)
+
+      write(lun,"(A)") "P3"
+      write(lun,"(I0,X,I0)") image_width, image_height
+      write(lun,"(I0)") pmax
+
+      do jj=1,image_height
+         do ii=1,image_width
+            write(lun,"(I0,X,I0,X,I0)") data(:,ii,jj)
+         end do
+      end do
+
+      close(lun)
+   end subroutine write_ppm_ascii
+
+   subroutine write_ppm_binary(filename,data,pixel_max)
+      character(len=*), intent(IN) :: filename
+      integer(int32), dimension(:,:,:), intent(IN) :: data
+      integer, intent(IN), optional :: pixel_max
+
+      integer :: lun, image_height, image_width, pmax
+      integer(int8), dimension(:,:,:), allocatable :: binary_data
+
+      pmax = 255
+      if(present(pixel_max)) pmax = pixel_max
+
+      image_width = size(data,2)
+      image_height = size(data,3)
+
+      open(newunit=lun,file=filename,status="replace",action="write",&
+      form="formatted")
+
+      write(lun,"(A)") "P6"
+      write(lun,"(I0,X,I0)") image_width, image_height
+      write(lun,"(I0)") pmax
+      close(lun)
+
+      open(newunit=lun,file=filename,status="old",action="write",&
+      form="unformatted",position="append")
+
+      write(lun) convert_to_unsigned(data)
+      close(lun)
+   end subroutine write_ppm_binary
 end program main
--- src/rtweekend.f90
+++ src/rtweekend.f90
@@ -11,7 +11,7 @@ module rtweekend
       integer(int8) :: n_u
       integer :: n_set_upper_bits
 
-      integer :: ii,bit_mask
+      integer :: bit_mask
       character(len=32) :: err
 
       bit_mask = ibits(n,bit_size(n_u),bit_size(n)-bit_size(n_u))
@@ -23,13 +23,6 @@ module rtweekend
          error stop "ERROR: BITWISE VALUE "//trim(adjustl(err))//" CANNOT FIT IN 8 BIT UNSIGNED INT"
       end if
 
-      n_u = 0
-
-      do ii = bit_size(n_u)-1,0,-1
-         if(n.ge.(2**ii)) then
-            n_u = ibset(n_u,ii)
-            n = n - 2**ii
-         end if
-      end do
+      n_u = iand(n,z'FF')
    end function convert_to_unsigned
 end module rtweekend
